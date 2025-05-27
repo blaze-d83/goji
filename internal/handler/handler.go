fb2eb2b10f49fcfa76002c7de31ddda2f9d62e7c
blaze-d83 <d.awareai83@gmail.com>
2025-05-27 09:29:43 +0000

    first internal and api

---
+++ internal/handler/handler.go
@@ -0,0 +1,9 @@
+package handler
+
+type Handler struct {
+	// Handler data
+}
+
+func NewHandler() *Handler {
+	return &Handler{}
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,3 +1,39 @@
 package server
 
+import (
+	"net/http"
 
+	"github.com/blaze-d83/goji/internal/config"
+)
+
+func New(cfg *config.Config) *http.Server {
+
+	// Declare a router
+	// For example:
+	// r := chi.NewRouter()
+
+	return &http.Server{
+		// Addr:                         "",
+		// Handler:                      nil,
+		// DisableGeneralOptionsHandler: false,
+		// TLSConfig:                    &tls.Config{},
+		// ReadTimeout:                  0,
+		// ReadHeaderTimeout:            0,
+		// WriteTimeout:                 0,
+		// IdleTimeout:                  0,
+		// MaxHeaderBytes:               0,
+		// TLSNextProto:                 map[string]func(*http.Server, *tls.Conn, http.Handler){},
+		// ConnState: func(net.Conn, http.ConnState) {
+		// 	panic("TODO")
+		// },
+		// ErrorLog: &log.Logger{},
+		// BaseContext: func(net.Listener) context.Context {
+		// 	panic("TODO")
+		// },
+		// ConnContext: func(ctx context.Context, c net.Conn) context.Context {
+		// 	panic("TODO")
+		// },
+		// HTTP2:     &http.HTTP2Config{},
+		// Protocols: &http.Protocols{},
+	}
+}
